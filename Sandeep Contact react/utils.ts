21618db370467cacd3e0a78104feee2548a9ead9
sandeepsingh0307 <130955507+sandeepsingh0307@users.noreply.github.com>
2024-06-04 12:46:41 +0530

    Delete Sandeep Contact react directory

--- Sandeep Contact react/utils.ts
+++
@@ -1,3 +0,0 @@
-// export const BASE_URL = "http://192.168.29.223:5002"; //home
-// export const BASE_URL = "http://192.168.1.9:5002"       //office
-export const BASE_URL = "https://contactwindos.azurewebsites.net"
